21a18c1b20f9f99c3b5ace41a0dd6ed4701023d7
Enrique Ramirez <enriquein@gmail.com>
2011-09-26 21:48:23 -0400

    Added data access class and data entity.

---
+++ TimesheetHelper/DbAccess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dapper;
+
+namespace TimesheetHelper
+{
+    public class DbAccess
+    {
+        private static string _connectionString = "Data Source =filename; Version =3;";
+
+        public static void SaveEvent(EventInfo eventInfo)
+        {}
+    }
+}
---
+++ TimesheetHelper/EventInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TimesheetHelper
+{
+    public class EventInfo
+    {
+        public int Id { get; set; }
+        public string WindowTitle { get; set; }
+        public DateTime EventDate { get; set; }
+        public string EventType { get; set; }
+    }
+}
