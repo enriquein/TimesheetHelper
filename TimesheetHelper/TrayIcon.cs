6ea3d20c8b0e7cd095ce6e0af28b6d5754bf5a4c
Enrique Ramirez <enriquein@gmail.com>
2011-09-26 23:31:40 -0400

    Changed hook events to use programmatic impl. Moved logic from form.

--- TimesheetHelper/TrayIcon.cs
+++ TimesheetHelper/TrayIcon.cs
@@ -12,35 +12,9 @@ namespace TimesheetHelper
 {
     public partial class TrayIcon : Form
     {
-        // We want to keep track of this so that we only save once each few seconds or so.
-        private DateTime _lastSave = DateTime.Now.AddMinutes(-1);
-        private int _saveDelay = 15; // measured in seconds.
-
         public TrayIcon()
         {
             InitializeComponent();
-        }
-
-        private void OnGlobalKeyPress(object sender, KeyPressEventArgs e)
-        {
-            SaveEvent("Keypress");
-        }
-
-        private void OnGlobalMouseClick(object sender, MouseEventArgs e)
-        {
-            SaveEvent("MouseClick");
-        }
-
-        private void SaveEvent(string eventType)
-        {
-            TimeSpan ts = DateTime.Now - _lastSave;
-            if (ts.TotalSeconds > _saveDelay)
-            {
-                DbAccess.SaveEvent(new EventInfo() { 
-                    WindowTitle = Win32.GetCurrentWindowTitle(), 
-                    EventType = eventType 
-                });
-            }
         }
     }
 }
