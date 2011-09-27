b0f69ca559be22d544f49e41dd21d710aa55d371
Enrique Ramirez <enriquein@gmail.com>
2011-09-26 21:54:08 -0400

    Implemented saving events.

--- TimesheetHelper/DbAccess.cs
+++ TimesheetHelper/DbAccess.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Dapper;
+using System.Data.SQLite;
 
 namespace TimesheetHelper
 {
@@ -11,6 +12,15 @@ namespace TimesheetHelper
         private static string _connectionString = "Data Source =filename; Version =3;";
 
         public static void SaveEvent(EventInfo eventInfo)
-        {}
+        {
+            using (var conn = new SQLiteConnection(_connectionString))
+            {
+                var query = @"  insert into EventInfo(WindowTitle, EventDate, EventType)
+                                values (@title, @date, @type);"; 
+                conn.Open();
+
+                conn.Execute(query, new { title = eventInfo.WindowTitle, date = eventInfo.EventDate, type = eventInfo.EventType });
+            }
+        }
     }
 }
