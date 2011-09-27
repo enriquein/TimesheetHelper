5ae5e31b846e8487444f0dc42273ee149d2d1d93
Enrique Ramirez <enriquein@gmail.com>
2011-09-27 01:58:19 -0400

    Added EventLogger class to act as intermediary between hook and db.

---
+++ TimesheetHelper/EventLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TimesheetHelper.Data;
+
+namespace TimesheetHelper
+{
+    public class EventLogger
+    {
+        private DateTime _lastUpdate;
+        private int _saveDelay = 5;
+        private GlobalKbHook _kbHook;
+
+        public EventLogger(GlobalKbHook kbHook)
+        {
+            _lastUpdate = DateTime.Now.AddMinutes(-10);
+            _kbHook = kbHook;
+        }
+
+        public void Log()
+        {
+            TimeSpan ts = DateTime.Now - _lastUpdate;
+            if (ts.TotalSeconds >= _saveDelay)
+            {
+                DbAccess.SaveEvent(new EventInfo(Win32.GetCurrentWindowTitle(), "Keypress"));
+            }
+        }
+    }
+}
