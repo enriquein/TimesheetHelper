2e49bedf795e2c28bdc0c9456b9961af789ee959
Enrique Ramirez <enriquein@gmail.com>
2011-09-26 23:06:39 -0400

    TrayIcon form now calls save method when event fires.

--- TimesheetHelper/TrayIcon.cs
+++ TimesheetHelper/TrayIcon.cs
@@ -6,24 +6,41 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using TimesheetHelper.Data;
 
 namespace TimesheetHelper
 {
     public partial class TrayIcon : Form
     {
-        public TrayIcon( /* something that can save the event */ )
+        // We want to keep track of this so that we only save once each few seconds or so.
+        private DateTime _lastSave = DateTime.Now.AddMinutes(-1);
+        private int _saveDelay = 15; // measured in seconds.
+
+        public TrayIcon()
         {
             InitializeComponent();
         }
 
         private void OnGlobalKeyPress(object sender, KeyPressEventArgs e)
         {
-            /* tell that something that the event happened */
+            SaveEvent("Keypress");
         }
 
         private void OnGlobalMouseClick(object sender, MouseEventArgs e)
         {
-            /* tell that something that the event happened */
+            SaveEvent("MouseClick");
+        }
+
+        private void SaveEvent(string eventType)
+        {
+            TimeSpan ts = DateTime.Now - _lastSave;
+            if (ts.TotalSeconds > _saveDelay)
+            {
+                DbAccess.SaveEvent(new EventInfo() { 
+                    WindowTitle = Win32.GetCurrentWindowTitle(), 
+                    EventType = eventType 
+                });
+            }
         }
     }
 }
